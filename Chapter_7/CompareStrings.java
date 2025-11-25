7e4f54a2b508c3016ae7bf5c96bc9c09d97c60e4
KeithBuscato-UCM <keithbuscato1105@gmail.com>
2025-11-26 00:11:23 +0800

    Add files via upload

---
+++ Chapter_7/CompareStrings.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class CompareStrings {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name > ");
+        String name = input.next();
+        StringBuilder sb = new StringBuilder(name);
+
+            String result = name.substring(0, 1).toUpperCase() 
+            + name.substring(1);
+            
+            if(sb.toString().equals(result)){
+                System.out.println(result + " is equal to " + sb);
+            }
+            else{
+                System.out.println(result + " is not equal to " + sb);
+            }
+    }
+}
