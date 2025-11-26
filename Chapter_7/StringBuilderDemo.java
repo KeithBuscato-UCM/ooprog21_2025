8eb1110f0cfc242c0ceaa86b167cc369dae7810c
KeithBuscato-UCM <keithbuscato1105@gmail.com>
2025-11-26 08:36:55 +0800

    Refactor StringBuilderDemo to use String variables

--- Chapter_7/StringBuilderDemo.java
+++ Chapter_7/StringBuilderDemo.java
@@ -1,18 +1,20 @@
 class StringBuilderDemo{
    public static void main(String[] args){
-   StringBuilder nameString = new StringBuilder("Barbara");
-   StringBuilder addressString = new StringBuilder("6311 Hickory Nut Grove Road");
+   String nameString = "Barbara"; 
+   StringBuilder ns = new StringBuilder("Barbara");
+   String addressString = "6311 Hickory Nut Grove Road";
+   StringBuilder as = new StringBuilder(addressString);
    
    System.out.println("(StringBuilder 1) nameString: "+ nameString);
-   System.out.println("Capacity of nameString is: " +nameString.capacity());
+   System.out.println("Capacity of nameString is: " +ns.capacity());
    
    System.out.println("(StringBuilder 2) addressString: "+ addressString);
-   System.out.println("Capacity of nameString is: " + addressString.capacity());
+   System.out.println("Capacity of nameString is: " + as.capacity());
    
-   nameString.setLength(20);
-   addressString.setLength(20);
+   ns.setLength(20);
+   as.setLength(20);
    
-   System.out.println("(Length set to 20 for StringBuilder 1) the name is " + nameString + "end");
-   System.out.println("(Length set to 20 for StringBuilder 2) the address is " + addressString);   
+   System.out.println("(Length set to 20 for StringBuilder 1) the name is " + ns + "end");
+   System.out.println("(Length set to 20 for StringBuilder 2) the address is " + as);   
    }
 }
